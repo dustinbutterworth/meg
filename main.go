7397739ce1422a576e2d7cc7c9fff519ab36dcab
Dustin Butterworth <51181391+dustinbutterworth@users.noreply.github.com>
2020-06-18 22:47:03 -0400

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	userAgent = "Mozilla/5.0 (compatible; meg/0.2; +https://github.com/tomnomnom/meg)"
+	userAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36"
 
 	// argument defaults
 	defaultPathsFile = "./paths"
